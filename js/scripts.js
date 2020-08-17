40f5ac59169879362d69ef7b5d7385953262c858
Mike Manchee <mikemanchee@gmail.com>
2020-08-17 14:03:40 -0700

    add js structure to project
    co-authored-by: Grant Eadie <grantleadie@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(doument).ready(function() {
+  let count = 11
+  $(#grocery-list).add(function() {
+    $("div#list").append('<input id="item' + count + '" class="form-control" type="text">"');
+    count++;
+  });
+  $
+});
