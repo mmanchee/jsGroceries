d3933e78ef8ca7df4c9a601c38a2d4ce29a47e28
Mike Manchee <mikemanchee@gmail.com>
2020-08-17 17:03:10 -0700

    add figured out multi button on project
    co-authored-by: Grant Eadie <grantleadie@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,35 @@
 $(document).ready(function() {
-  let items = ["item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"];
+  let items = ["item1"];
   let userInput = [];
-  // let count = 10;
-  // let idCount;
-  // let groceryArray = [];
+  let count = 1;
+  let idCount;
 
-  // $("#add-line").submit(function(event) {
-  //   count++;
-  //   idCount = "item" + count;    
-  //   $("div.list").append("<input id=" + idCount + " class='form-control' type='text'>");
-  //   groceryArray.push(idCount);  
-  //   event.preventDefault();
-  // });
+  $("#add-line").click(function(event) {
+    count++;
+    idCount = "item" + count;    
+    $("div.list").append("<input id=" + idCount + " class='form-control' type='text'>");
+    items.push(idCount);  
+    event.preventDefault();
+  });
 
-  $("#grocery-list").submit(function(event) {
+  $("#submit").click(function(event) {
     items.forEach(function(item) {
       userInput.push($("input#" + item).val());
     });
     
     userInput.sort();
-    alert(userInput);
 
     userInput.forEach(function(item) {
       if (item !== "") {
+        item = item.toUpperCase();
         $("#itemOutput").append("<li>" + item + "</li>");
-      }
-      
-    })
-    // 
-      
-      
-    //   item = $("#" + id).val();
-    //   alert(item);
-    //   if (item !== " ") {
-    //     groceryArray.push(item);
-    //   };
-    //   alert(groceryArray);
+      }      
+    });
 
+    $("#list").show();
+    $(".grocery-list").hide();
 
     event.preventDefault();
-    //grocArray.sort();
-    //alert(grocArray);
-    //grocArray.forEach(function() {
-
-    //});
+ 
   });
 });
